e2a3e26bf8f1d9c2463d0716739329d6ef6aa567
Simon Zelenski <simon.zelenski@gmail.com>
2023-09-13 14:14:34 +0200

    Add mongodb gateway functions

--- root_orchestrator/system-manager-python/ext_requests/mongodb_client.py
+++ root_orchestrator/system-manager-python/ext_requests/mongodb_client.py
@@ -8,6 +8,7 @@ MONGO_PORT = os.environ.get('CLOUD_MONGO_PORT', 10007)
 MONGO_ADDR_CLUSTERS = 'mongodb://' + str(MONGO_URL) + ':' + str(MONGO_PORT) + '/clusters'
 MONGO_ADDR_JOBS = 'mongodb://' + str(MONGO_URL) + ':' + str(MONGO_PORT) + '/jobs'
 MONGO_ADDR_USERS = 'mongodb://' + str(MONGO_URL) + ':' + str(MONGO_PORT) + '/users'
+MONGO_ADDR_GATEWAYS = 'mongodb://' + str(MONGO_URL) + ':' + str(MONGO_PORT) + '/gateways'
 
 mongo_clusters = None
 mongo_jobs = None
@@ -16,13 +17,17 @@ mongo_applications = None
 mongo_services = None
 mongo_organization = None
 
+mongo_gateway = None
+mongo_gateway_nodes = None
+mongo_gateway_services = None
+
 app = None
 
 CLUSTERS_FRESHNESS_INTERVAL = 45
 
 
 def mongo_init(flask_app):
-    global app, mongo_clusters, mongo_jobs, mongo_users, mongo_applications, mongo_services, mongo_organization
+    global app, mongo_clusters, mongo_jobs, mongo_users, mongo_applications, mongo_services, mongo_organization, mongo_gateway, mongo_gateway_nodes, mongo_gateway_services
 
     app = flask_app
 
@@ -32,6 +37,10 @@ def mongo_init(flask_app):
     mongo_organization = PyMongo(app, uri=MONGO_ADDR_USERS).db['organization']
     mongo_applications = mongo_jobs.db['apps']
     mongo_services = mongo_jobs.db['jobs']
+
+    mongo_gateway = PyMongo(app, uri=MONGO_ADDR_GATEWAYS)
+    mongo_gateway_nodes = mongo_gateway.db['nodes']
+    mongo_gateway_services = mongo_gateway.db['services']
 
     app.logger.info("MONGODB - init mongo")
     app.logger.info(mongo_users)
---
+++ root_orchestrator/system-manager-python/services/gateway_management.py
@@ -0,0 +1,45 @@
+import logging 
+import ipaddress
+import threading
+
+from ext_requests.gateway_db import *
+from ext_requests.cluster_requests import cluster_request_to_deploy_gateway, cluster_request_to_update_gateway
+from ext_requests.net_plugin_requests import net_inform_firewall_deploy
+from sla.versioned_sla_parser import parse_sla_json
+
+
+def create_service_gateway(current_user, sla):
+    data = parse_sla_json(sla)
+    logging.log(logging.INFO, sla)
+    services = data.get('microservices')
+
+    for microservice in services:
+        if microservice.get("microserviceID") is None:
+            return {'message': "microserviceID cannot be empty"}, 404
+        service = mongo_get_service_instances_by_id(microservice["microserviceID"])
+        if service is None:
+            return {'message': "service {} not found".format(microservice["microserviceID"])}, 404
+        microservice["internal_port"] = service["port"] # internal port
+        duplicate = mongo_get_gateway_service_by_id(microservice["microserviceID"])
+        if duplicate is not None:
+            return {'message': "service {} already exposed".format(microservice["microserviceID"])}, 404
+        mongo_add_service_to_gatewaydb(microservice)
+        microservice['_id'] = str(microservice['_id'])
+        # check for every instance deployed, if worker node is publicly accessable
+        # if yes, deploy firewall
+        # if no, deploy gateway inside cluster on available public worker
+        instances = service.get("instance_list")
+        clusters = mongo_get_clusters_of_active_service_instances(microservice['microserviceID'])
+        for cluster in clusters:
+            # notify cluster to enable gateway for microservice if possible
+            cluster_request_to_deploy_gateway(cluster, microservice)
+
+    return {'message': 'service(s) successfully exposed'}, 200
+
+
+def get_service_gateway(user, service_id):
+    return
+
+def delete_service_gateway(user, service_id):
+    return
+
