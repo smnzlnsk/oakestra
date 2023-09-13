fb103f18e6c47cb60aed58b4006a244cc271e2b5
Simon Zelenski <simon.zelenski@gmail.com>
2023-09-13 17:04:37 +0200

    Fix some addressing issues and add IDPv2 docker-compose for consistent IPv6 addressing between services

--- root_orchestrator/system-manager-python/ext_requests/cluster_requests.py
+++ root_orchestrator/system-manager-python/ext_requests/cluster_requests.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import requests
+from ipaddress import ip_address, IPv6Address
 
 from ext_requests.apps_db import mongo_find_job_by_id, mongo_find_cluster_of_job
 from ext_requests.cluster_db import mongo_find_cluster_by_id, mongo_find_cluster_by_ip
@@ -11,8 +12,11 @@ def cluster_request_to_deploy(cluster_id, job_id, instance_number):
     cluster = mongo_find_cluster_by_id(cluster_id)
     job = mongo_find_job_by_id(job_id)
     # adjusted cluster_addr for ipv6
+    ip = cluster.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
     try:
-        cluster_addr = 'http://[' + cluster.get('ip') + ']:' + str(cluster.get('port')) + '/api/deploy/' + str(job_id) + "/" + str(instance_number)
+        cluster_addr = 'http://' + url + ':' + str(cluster.get('port')) + '/api/deploy/' + str(job_id) + "/" + str(instance_number)
         job['_id'] = str(job['_id'])
         resp = requests.post(cluster_addr, json=job)
         print(resp)
@@ -22,8 +26,11 @@ def cluster_request_to_deploy(cluster_id, job_id, instance_number):
 
 def cluster_request_to_delete_job(job_id, instance_number):
     cluster = mongo_find_cluster_of_job(job_id, int(instance_number))
+    ip = cluster.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
     try:
-        cluster_addr = 'http://[' + cluster.get('ip') + ']:' + str(cluster.get('port')) + '/api/delete/' + str(
+        cluster_addr = 'http://' + url + ':' + str(cluster.get('port')) + '/api/delete/' + str(
             job_id) + "/" + str(instance_number)
         resp = requests.get(cluster_addr)
         print(resp)
@@ -36,7 +43,10 @@ def cluster_request_to_delete_job(job_id, instance_number):
 def cluster_request_to_delete_job_by_ip(job_id, instance_number,ip):
     try:
         cluster = mongo_find_cluster_by_ip(ip)
-        cluster_addr = 'http://[' + cluster.get('ip') + ']:' + str(cluster.get('port')) + '/api/delete/' + str(
+        ipaddr = cluster.get('ip')
+        # check if ip is IPv6 and add brackets
+        url = '[{}]'.format(ipaddr) if type(ip_address(ipaddr)) is IPv6Address else ipaddr
+        cluster_addr = 'http://' + url+ ':' + str(cluster.get('port')) + '/api/delete/' + str(
             job_id) + "/" + str(instance_number)
         resp = requests.get(cluster_addr)
         print(resp)
@@ -46,7 +56,10 @@ def cluster_request_to_delete_job_by_ip(job_id, instance_number,ip):
 
 
 def cluster_request_to_replicate_up(cluster_obj, job_obj, int_replicas):
-    cluster_addr = 'http://[' + cluster_obj.get('ip') + ']:' + str(cluster_obj.get('port')) + '/api/replicate/'
+    ip = cluster_obj.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
+    cluster_addr = 'http://' + url + ':' + str(cluster_obj.get('port')) + '/api/replicate/'
     try:
         resp = requests.post(cluster_addr, json={'job': job_obj, 'int_replicas': int_replicas})
         print(resp)
@@ -56,7 +69,10 @@ def cluster_request_to_replicate_up(cluster_obj, job_obj, int_replicas):
 
 
 def cluster_request_to_replicate_down(cluster_obj, job_obj, int_replicas):
-    cluster_addr = 'http://[' + cluster_obj.get('ip') + ']:' + str(cluster_obj.get('port')) + '/api/replicate/'
+    ip = cluster_obj.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
+    cluster_addr = 'http://' + cluster_obj.get('ip') + ':' + str(cluster_obj.get('port')) + '/api/replicate/'
     try:
         resp = requests.post(cluster_addr, json={'job': job_obj, 'int_replicas': int_replicas})
         print(resp)
@@ -66,7 +82,10 @@ def cluster_request_to_replicate_down(cluster_obj, job_obj, int_replicas):
 
 
 def cluster_request_to_move_within_cluster(cluster_obj, job_id, node_from, node_to):
-    cluster_addr = 'http://[' + cluster_obj.get('ip') + ']:' + str(cluster_obj.get('port')) + '/api/move/'
+    ip = cluster_obj.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
+    cluster_addr = 'http://' + url + ':' + str(cluster_obj.get('port')) + '/api/move/'
     try:
         resp = requests.post(cluster_addr, json={'job': job_id, 'node_from': node_from, 'node_to': node_to})
         print(resp)
@@ -83,8 +102,11 @@ def cluster_request_to_deploy_gateway(cluster_id, microservice):
     print(json_data)
 
     # adjusted cluster_addr for ipv6
+    ip = cluster.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
     try:
-        cluster_addr = 'http://[' + cluster.get('ip') + ']:' + str(cluster.get('port')) + '/api/gateway/deploy'
+        cluster_addr = 'http://' + url + ':' + str(cluster.get('port')) + '/api/gateway/deploy'
         #gateway['_id'] = str(gateway['_id'])
         resp = requests.post(cluster_addr, json=json_data)
         print(resp)
@@ -97,8 +119,11 @@ def cluster_request_to_update_gateway(cluster_id, gateway_id, microservice):
     cluster = mongo_find_cluster_by_id(cluster_id)
     gateway = mongo_find_gateway_by_id(gateway_id)
     # adjusted cluster_addr for ipv6
+    ip = cluster.get('ip')
+    # check if ip is IPv6 and add brackets
+    url = '[{}]'.format(ip) if type(ip_address(ip)) is IPv6Address else ip
     try:
-        cluster_addr = 'http://[' + cluster.get('ip') + ']:' + str(cluster.get('port')) + '/api/gateway/update/' + str(gateway_id)
+        cluster_addr = 'http://' + url + ':' + str(cluster.get('port')) + '/api/gateway/update/' + str(gateway_id)
         gateway['_id'] = str(gateway['_id'])
         resp = requests.post(cluster_addr, json=microservice)
         print(resp)
